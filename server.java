47191cf50471d02d4342ab7c023c841fa37f93dd
xoxo <48424752+nrlfrthh@users.noreply.github.com>
2019-06-12 06:35:31 +0800

    Update server.java

--- server.java
+++ server.java
@@ -1,233 +1,50 @@
 require 'socket'
 
-public class Server implements CryptoInterface {
-	
-	private GUIInterface m_gui;
-	
-	private static final String SERVER_IDENTIFICATION = "server"; 
-	
-	private static final byte[] SALT = {(byte)0xa0, (byte)0x4e, (byte)0x2b, (byte)0x92, (byte)0x4a, (byte)0xd6, (byte)0x59, (byte)0x86};
-	
-	private static final int ITERATION_COUNT = 25;
-	private Socket connectionSocket = null;
-	private ServerSocket serverSocket =null;
-	private String sharedSecretKey;
-	private Cipher communicationEncryptionCipher;
-	private Cipher communicationDecryptionCipher;
-	private InputStream inputStream;
-	private OutputStream outputStream;
-	private String clientId;
-	
-	public Server(GUIInterface gui)
-	{
-		m_gui = gui;
-	}
-	
-	@Override
-	public void closeServer()
-	{		
-	}
-	
-	@Override
-	public void endConnection() 
-	{
-	}
-	@Override
-	public void receiveMessages()
-	{
-		while(true)
-		{
-			try
-			{
-				byte[] clientInput = readFromClient(inputStream);
-				m_gui.puts("Receiving encrypted bytes: " + Helpers.ByteToString(clientInput));
-				String clientInputDecrypted = new String(communicationDecryptionCipher.doFinal(clientInput));
-				m_gui.puts( clientId + " > " + clientInputDecrypted);
-			}
-			catch(Exception e )
-			{
-				
-			}
-		}
-	}
+class Server
+   def initialize(socket_address, socket_port)
+      @server_socket = TCPServer.open(socket_port, socket_address)
 
-	@Override
-	public boolean sendMessage(String s) 
-	{
-		try
-		{
-			m_gui.puts("Sending Message: " + s );
-			byte[] bytes = s.getBytes();
-			m_gui.puts("Bytes: " + Helpers.ByteToString(bytes) );
-			byte[] encrypted = communicationEncryptionCipher.doFinal(bytes);
-			m_gui.puts("Sending Encrypted bytes: " + Helpers.ByteToString(encrypted) );
-			writeToClient(outputStream, encrypted);
-			m_gui.printf(SERVER_IDENTIFICATION + " > " + s);
-		}
-		catch(Exception e )
-		{
-			return false;
-		}
-		return true;
-	}
+      @connections_details = Hash.new
+      @connected_clients = Hash.new
 
-	@Override
-	public boolean startClient(String host, int port, String sharedKey) 
-	{
-		throw new RuntimeException("Not Implemented");
-	}
-	
-	/*protected void startConnection()
-	{
-		m_gui.connectionReady();
-	}*/
+      @connections_details[:server] = @server_socket
+      @connections_details[:clients] = @connected_clients
 
-	@Override
-	public boolean startServer(int port, String sharedKey) {
-		
-		try
-		{
-			sharedSecretKey = sharedKey;
-			serverSocket = new ServerSocket(port);
-		}
-		catch(IOException e)
-		{
-			m_gui.puts("Server Error: " + e.getMessage() );
-			return false;
-		}
-		
-		return true;
-	}
-	
-	@Override
-	public void acceptConnection()
-	{
-		
-		try
-		{
-			if( serverSocket != null )
-				connectionSocket = serverSocket.accept();
-			
-			if(connectionSocket == null ) m_gui.connectionClosed();
-			
-			 inputStream = connectionSocket.getInputStream();
-			outputStream = connectionSocket.getOutputStream();
-			//BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
-			
-			DiffieHellmanHelper diffieHellmanHelper = new DiffieHellmanHelper();
-			
-			// Receive identification data from client.
-			byte[] identificationData = readFromClient(inputStream);
-			String identificationString = new String(identificationData);
-			m_gui.puts("Receiving handshake message: " + identificationString);
-			
-			String[] identificationArray = identificationString.split(",");;
-			clientId = identificationArray[0];
-			String clientChallenge = identificationArray[1];
-			m_gui.puts(clientId + " has connected");
-			m_gui.puts("Client challenge" + clientChallenge);
-			
-			// Generate Diffie-Hellman keypair, and send data to client to continue handshake. 
-			DHParameterSpec diffieHellmanParameters = diffieHellmanHelper.generateDiffieHellmanParameters();
-	        m_gui.puts("Generating Diffie Hellman key pair for server");
-	        KeyPair serverKeyPair = diffieHellmanHelper.createKeyPair(diffieHellmanParameters);
-	        
-	        byte[] serverPublicKeyEncoded = serverKeyPair.getPublic().getEncoded();
-	        
-	        String serverChallenge = java.util.UUID.randomUUID().toString();
-	        m_gui.puts("Sending server challenge: " + serverChallenge);
-	        byte[] encryptedServerIdentification = encryptHandshake(sharedSecretKey, SERVER_IDENTIFICATION.getBytes(), Cipher.ENCRYPT_MODE);
-	        writeToClient(outputStream, serverChallenge.getBytes());
-	        m_gui.puts("Sending id: " + SERVER_IDENTIFICATION);
-	        writeToClient(outputStream, encryptedServerIdentification);
-	        m_gui.puts("Sending client challenge: " + clientChallenge);
-	        writeToClient(outputStream, encryptHandshake(sharedSecretKey, clientChallenge.getBytes(), Cipher.ENCRYPT_MODE));
-	        m_gui.puts("Sending server session Key Pair: " + serverKeyPair);
-	        writeToClient(outputStream, encryptHandshake(sharedSecretKey, serverPublicKeyEncoded, Cipher.ENCRYPT_MODE));
-	
-	        byte[] identificationData2 = readFromClient(inputStream);
-	        byte[] serverChallengeData = readFromClient(inputStream);
-	        byte[] clientPublicKeyBytesEncoded = readFromClient(inputStream);
-	        
-	        String clientId2 = new String(encryptHandshake(sharedSecretKey, identificationData2, Cipher.DECRYPT_MODE));
-	        if (!clientId2.equals(clientId)) {
-	        	m_gui.puts("Incorrect client has connected, terminating program");
-	        	m_gui.connectionClosed();
-	        }
-	        String serverChallengeReturned = new String(encryptHandshake(sharedSecretKey, serverChallengeData, Cipher.DECRYPT_MODE));
-	        if (!serverChallengeReturned.equals(serverChallenge)) {
-	        	m_gui.puts("Incorrect server challenge returned, terminating program");
-	        	m_gui.connectionClosed();
-	        }
-	        byte[] unencryptedEncodedPublicKeyBytes = encryptHandshake(sharedSecretKey, clientPublicKeyBytesEncoded, Cipher.DECRYPT_MODE);
-	        PublicKey clientPublicKey = diffieHellmanHelper.getUnencodedPublicKey(unencryptedEncodedPublicKeyBytes);
-			diffieHellmanHelper.getKeyAgreement().doPhase(clientPublicKey, true);
-			
-			byte[] serverSharedKey = diffieHellmanHelper.getKeyAgreement().generateSecret();
-			m_gui.puts("The shared key is: ");
-			m_gui.puts(DatatypeConverter.printHexBinary(serverSharedKey));
-			
-			// Handshake is done, now encrypt all user input in DES format using shared secret key.
-			diffieHellmanHelper.getKeyAgreement().doPhase(clientPublicKey, true);
-			SecretKey serverDESKey = diffieHellmanHelper.getKeyAgreement().generateSecret("DES");
-			communicationEncryptionCipher = getCommunicationCipher(Cipher.ENCRYPT_MODE, serverDESKey);
-			communicationDecryptionCipher = getCommunicationCipher(Cipher.DECRYPT_MODE, serverDESKey);
-			m_gui.connectionReady();
-				
-		}
-		catch(Exception e )
-		{
-			m_gui.connectionClosed();;
-		}
-	}
-	
-	/*public static void main(String args[]) throws Exception {
-		
-		String userInput;
-		while (true) {
-			byte[] clientInput = readFromClient(inputStream);
-			String clientInputDecrypted = new String(communicationDecryptionCipher.doFinal(clientInput));
-			puts(clientId + " sent: " + clientInputDecrypted);
-			puts(">>");
-			userInput = userInputReader.readLine();
-			writeToClient(outputStream, communicationEncryptionCipher.doFinal(userInput.getBytes()));
-		}
-	
-	}*/
-	
-	private byte[] readFromClient(InputStream inputStream) throws IOException {
-		byte[] lenMetaData = new byte[4];
-		inputStream.read(lenMetaData, 0, 4);
-		ByteBuffer metaDataBuffer = ByteBuffer.wrap(lenMetaData);
-		int lengthMetaDataInt = metaDataBuffer.getInt();
-		byte[] dataBuffer = new byte[lengthMetaDataInt];
-		inputStream.read(dataBuffer);
-		return dataBuffer;
-	}
-	
-	private void writeToClient(OutputStream outputStream, byte[] dataBuffer) throws IOException {
-		ByteBuffer metaDataBuffer = ByteBuffer.allocate(4);
-		metaDataBuffer.putInt(dataBuffer.length);
-		outputStream.write(metaDataBuffer.array());
-		outputStream.write(dataBuffer);
-		outputStream.flush();
-	}
-	
-	// Taken from http://docs.oracle.com/javase/1.4.2/docs/guide/security/jce/JCERefGuide.html#PBEEx
-	private byte[] encryptHandshake(String sharedSecretKey, byte[] plainText, int encryption_mode) throws InvalidKeySpecException, NoSuchAlgorithmException, 
-			NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-		PBEParameterSpec handshakeParamSpec = new PBEParameterSpec(SALT, ITERATION_COUNT);
-		PBEKeySpec handshakeKeySpec = new PBEKeySpec(sharedSecretKey.toCharArray());
-		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-		SecretKey pbeKey = keyFactory.generateSecret(handshakeKeySpec);
-		Cipher handshakeCipher = Cipher.getInstance("PBEWithMD5AndDES");
-		handshakeCipher.init(encryption_mode, pbeKey, handshakeParamSpec);
-		return handshakeCipher.doFinal(plainText);
-	}
+      puts 'Started server.........'
+      run
 
-	private Cipher getCommunicationCipher(int encryption_mode, SecretKey secretKey) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
-		Cipher serverCipherForEncryption = Cipher.getInstance("DES/ECB/PKCS5Padding");
-		serverCipherForEncryption.init(encryption_mode, secretKey);
-		return serverCipherForEncryption;
-	}
-}
+   end
+
+   def run
+      loop{
+         client_connection = @server_socket.accept
+         Thread.start(client_connection) do |conn| # open thread for each accepted connection
+            conn_name = conn.gets.chomp.to_sym
+            if(@connections_details[:clients][conn_name] != nil) # avoid connection if user exits
+               conn.puts "This username already exist"
+               conn.puts "quit"
+               conn.kill self
+            end
+
+            puts "Connection established #{conn_name} => #{conn}"
+            @connections_details[:clients][conn_name] = conn
+            conn.puts "Connection established successfully #{conn_name} => #{conn}, you may continue with chatting....."
+
+            establish_chatting(conn_name, conn) # allow chatting
+         end
+      }.join
+   end
+
+   def establish_chatting(username, connection)
+      loop do
+         message = connection.gets.chomp
+         puts @connections_details[:clients]
+         (@connections_details[:clients]).keys.each do |client|
+            @connections_details[:clients][client].puts "#{username} : #{message}"
+         end
+      end
+   end
+end
+
+
+Server.new( 8080, "localhost" )
